ae236a8e7c26a417ded04ed42dc70c4296c87959
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-20 19:45:27 -0300

    feat: created author repository

---
+++ src/Feed.Data/Repository/AuthorRepository.cs
@@ -0,0 +1,23 @@
+﻿using Feed.Business.Interfaces.Repositories;
+using Feed.Business.Models;
+using Feed.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feed.Data.Repository;
+
+public class AuthorRepository : Repository<Author>, IAuthorRepository
+{
+    public AuthorRepository(FeedDbContext context) : base(context)
+    {
+
+    }
+
+    public async Task<Author> GetAuthorPosts(Guid id)
+    {
+        return await Db.Authors
+            .AsNoTracking()
+            .Include(a => a.Posts)
+            .FirstOrDefaultAsync(a => a.Id == id)
+            ?? new Author();
+    }
+}
