c679d13e174e43b8c041af183d82fba0308e4461
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-21 22:12:21 -0300

    feat: create main controller

---
+++ src/Feed.Api/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using Feed.Api.DTOs;
+using Feed.Business.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feed.Api.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorController : MainController
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorDTO>>> Get()
+        {
+            var author = await _authorRepository.GetAll();
+            return Ok(author);
+        }
+    }
+}
