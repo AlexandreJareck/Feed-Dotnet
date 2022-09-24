7e7dd5d7710219189861f195d73b9868f7174bf1
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-23 23:16:46 -0300

    feat: created Login user DTO

---
+++ src/Feed.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Feed.Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace Feed.Api.Controllers
+{
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotifier notifier,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager
+                              ) : base(notifier)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+    }
+}
