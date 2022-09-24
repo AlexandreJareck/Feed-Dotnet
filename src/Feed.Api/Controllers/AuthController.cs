96f7723689390b9f96c72b22d4d74db1a830549f
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-23 23:37:29 -0300

    feat: add method login

--- src/Feed.Api/Controllers/AuthController.cs
+++ src/Feed.Api/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
-﻿using Feed.Business.Interfaces;
+﻿using Feed.Api.DTOs.User;
+using Feed.Business.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Feed.Api.Controllers
 {
+    [Route("api")]
     public class AuthController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -15,6 +18,56 @@ namespace Feed.Api.Controllers
         {
             _signInManager = signInManager;
             _userManager = userManager;
+        }
+
+        [HttpPost("new-account")]
+        public async Task<ActionResult> Register(RegisterUserDTO registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotifyError(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserDTO loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse("Ok");
+            }
+
+            if (result.IsLockedOut)
+            {
+                NotifyError("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            NotifyError("Usuário ou Senha incorretos");
+            return CustomResponse(loginUser);
         }
     }
 }
