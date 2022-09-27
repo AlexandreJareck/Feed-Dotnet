cc6d479561ba440226d7f1d789bc5fde8cc02302
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-26 21:59:42 -0300

    feat: use swagger in program

--- src/Feed.Api/Program.cs
+++ src/Feed.Api/Program.cs
@@ -1,25 +1,34 @@
 using Feed.Api.Configuration;
 using Feed.Data.Context;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSwaggerGen();
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<FeedDbContext>(x => x.UseSqlServer(connectionString));
+builder.Services.AddDbContext<FeedDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddApiConfig();
+builder.Services.AddSwaggerConfig();
 builder.Services.ResolveDependencies();
 
 var app = builder.Build();
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 app.UseApiConfig(app.Environment);
-
+app.UseSwaggerConfig(apiVersionDescriptionProvider);
 app.MapControllers();
 
 app.Run();
