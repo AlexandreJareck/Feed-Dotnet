cb672f69c4ba994eaad8deb42ae56df54e99cb4e
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-26 23:38:25 -0300

    fix(IAuthorService): add nullable in method add

--- src/Feed.Business/Interfaces/Services/IAuthorService.cs
+++ src/Feed.Business/Interfaces/Services/IAuthorService.cs
@@ -4,7 +4,7 @@ namespace Feed.Business.Interfaces.Services;
 
 public interface IAuthorService : IDisposable
 {
-    Task<Author> Add(Author author);
+    Task<Author?> Add(Author author);
     Task Update(Author author);
     Task Remove(Guid id);
 
