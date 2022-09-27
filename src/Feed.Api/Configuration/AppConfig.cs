0352e56b4c3c128c287a21acc2dba68485eee0ad
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-26 22:08:22 -0300

    feat: config versioning api

--- src/Feed.Api/Configuration/AppConfig.cs
+++ src/Feed.Api/Configuration/AppConfig.cs
@@ -8,6 +8,19 @@ public static class AppConfig
         services.AddControllers();
         services.AddEndpointsApiExplorer();
 
+        services.AddApiVersioning(options =>
+        {
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+        });
+
+        services.AddVersionedApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
         services.Configure<ApiBehaviorOptions>(options =>
         {
             options.SuppressModelStateInvalidFilter = true;
@@ -52,6 +65,11 @@ public static class AppConfig
         app.UseRouting();
         app.UseAuthentication();
         app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
 
         return app;
     }
