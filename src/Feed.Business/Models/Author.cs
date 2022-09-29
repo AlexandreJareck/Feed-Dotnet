5718534c3ad0729773e06520ab0048812ee1c140
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-28 22:49:08 -0300

    feat: add method validate to test in author

--- src/Feed.Business/Models/Author.cs
+++ src/Feed.Business/Models/Author.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 
 namespace Feed.Business.Models;
 
@@ -9,15 +10,38 @@ public class Author : Entity
     public string Role { get; set; }
 
     public IEnumerable<Post>? Posts { get; set; }
+
+    public ValidationResult Validate()
+    {
+        return new AuthorValidation().Validate(this);
+    }
 }
 
 public class AuthorValidation : AbstractValidator<Author>
 {
+
+    public static string RequiredErrorMsg => "O campo {PropertyName} precisa ser fornecido";
+    public static string MaxMinErrorMsg => "O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres";
+
     public AuthorValidation()
-    {
+    {        
+
         RuleFor(f => f.Name)
-                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(2, 100)
-                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+            .NotEmpty()
+            .WithMessage(RequiredErrorMsg)
+            .Length(2, 100)
+            .WithMessage(MaxMinErrorMsg);
+
+        RuleFor(f => f.AvatarUrl)
+            .NotEmpty()
+            .WithMessage(RequiredErrorMsg)
+            .Length(10, 100)
+            .WithMessage(MaxMinErrorMsg);
+
+        RuleFor(f => f.Role)
+            .NotEmpty()
+            .WithMessage(RequiredErrorMsg)
+            .Length(2, 100)
+            .WithMessage(MaxMinErrorMsg);
     }
 }
